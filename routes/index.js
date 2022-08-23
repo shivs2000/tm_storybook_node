a014ee7597d9ec2b41ead2cac527e06e56bdf04f
shivs2000 <singhshivam0666@gmail.com>
2022-08-23 21:54:34 +0530

    added user & story query,updated dashboard.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+//importing story model
+const Story = require("../models/Story");
+const User = require("../models/User");
+
 //@ desc Login/landing page
 //@ route  GET/
 router.get("/", ensureGuest, (req, res) => {
@@ -12,9 +16,20 @@ router.get("/", ensureGuest, (req, res) => {
 //@ desc Dashboard Landing page
 //@ route GET/
 
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard", {
-    name: req.user,
-  }); // always name this in lower case
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.findOne({ user: req.user.id });
+    console.log(req.user);
+    const userInfo = await User.findById(req.user.id);
+    console.log(userInfo);
+    res.render("dashboard", {
+      name: userInfo.firstName,
+      stories,
+    }); // always name this in lower case
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+//@
 module.exports = router;
